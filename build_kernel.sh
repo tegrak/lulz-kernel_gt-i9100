4c71478350d334cbf8a560f433188e7f1dbea916
tegrak <luciferanna@gmail.com>
2011-07-10 14:32:00 +0900

    build

--- build_kernel.sh
+++ build_kernel.sh
@@ -9,7 +9,10 @@ TOOLCHAIN="$TOP_DIR/toolchain/arm-eabi-4.4.0/bin/arm-eabi-"
 ROOTFS_PATH="$TOP_DIR/root"
 
 # Copy Kernel Configuration File
-cp -f $KERNEL_PATH/arch/arm/configs/c1_defconfig $KERNEL_PATH/.config
+# SHW-M250S
+# cp -f $KERNEL_PATH/arch/arm/configs/c1_defconfig $KERNEL_PATH/.config
+# GT-I9100
+# cp -f $KERNEL_PATH/arch/arm/configs/c1_rev02_defconfig $KERNEL_PATH/.config
 
 make -C $KERNEL_PATH oldconfig || exit -1
 make -C $KERNEL_PATH ARCH=arm CROSS_COMPILE=$TOOLCHAIN CONFIG_INITRAMFS_SOURCE="$ROOTFS_PATH" || exit -1
